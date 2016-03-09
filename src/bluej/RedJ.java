9f863a65110d72b9a5f160ce50993f57116a073a
root <root@ZedHack>
2016-03-09 16:26:49 +0300

    So nimecheki the project and dint get what it was all about so i wrote a siple one hehehehe

---
+++ src/bluej/RedJ.java
@@ -0,0 +1,16 @@
+
+package bluej;
+
+/**
+ *
+ * @author Mista Petroz <mista.petroz at zedfliq.net>
+ */
+public class RedJ {
+    /*
+ * That program is kinda kwul so i am going to copy it here
+ */
+    public static void main(String[] args){
+        System.out.print("\n\n\n\n\n\n\n WTF is this program going to do??? \n\n\n\n");
+        
+    }
+}
